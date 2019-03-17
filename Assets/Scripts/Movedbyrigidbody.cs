74c4b3dac77ac41092660b451092ef39dea1be96
Leandro Marques <leandrojerse@gmail.com>
2019-03-17 10:11:27 -0300

    Controlando o movimento do personagem com velocidade do Rb

---
+++ Assets/Scripts/Movedbyrigidbody.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movedbyrigidbody : MonoBehaviour
+{
+    [SerializeField]
+    private float SpeedFactor = 10f ;
+    
+    private Rigidbody2D Rb ;
+    void Start()
+    {
+        Rb= GetComponent<Rigidbody2D>();
+        
+    }
+
+    
+    void Update()
+    {
+        float HorizontalInput = Input.GetAxisRaw("Horizontal");
+        float VerticalInput = Input.GetAxisRaw("Vertical");
+        Rb.velocity =new Vector3(HorizontalInput, VerticalInput, 0f) * SpeedFactor;
+    }
+}
