44988dae59d3bb3e9882bd0ca7f14929e7dbb57f
Leandro Marques <leandrojerse@gmail.com>
2019-04-22 10:57:14 -0300

    CmeraFollow

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -33,7 +33,7 @@ public class PlayerControl : MonoBehaviour
            
            StopMusicAndTape();
            AudioManager.instance.PlaySoundLevelComplete(gameObject);
-            Destroy(gameObject);
+            DestroyPlayer();
             LevelManager.instance.ShowLevelCompletePanel();
             
         }
@@ -53,12 +53,16 @@ public class PlayerControl : MonoBehaviour
             StopMusicAndTape();
             AudioManager.instance.PlaySoundFail(gameObject);
             SFXManager.instance.ShowDieParticles(gameObject);
-            Destroy(gameObject);
+            DestroyPlayer();
             LevelManager.instance.ShowGameOverPanel();
     }
 
      void Impulse (float force) {
          rb.velocity = Vector3.zero;
          rb.AddForce(Vector3.up * force, ForceMode2D.Impulse);
+     }
+     void DestroyPlayer(){
+         Camera.main.GetComponent<CameraFollow>().TurnOff();
+         Destroy(gameObject);
      }
 } 
